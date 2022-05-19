9d337c099d7fecb78b09e2cd53948d96fbd95479
gongshu <1078419631@qq.com>
2022-05-19 21:42:04 +0800

    修复成绩统计

--- src/views/teacher/GradeAnalysis.vue
+++ src/views/teacher/GradeAnalysis.vue
@@ -1,16 +1,37 @@
 <template>
     <div>
-        <Analysis ></Analysis>
+        <el-collapse>
+            <el-collapse-item v-for="(v,k,i) in details" :key="i">
+                <template #title>
+                    <div>{{v['question_message']}}</div>
+                    <div><el-tag>错误率:{{v['rate']}}%</el-tag><el-tag>错误数:{{v['list'].length}}</el-tag></div>
+                    
+                </template>
+                <ul>
+                    <li>A:{{v['option_A']}}</li>
+                    <li>B:{{v['option_B']}}</li>
+                    <li>C:{{v['option_C']}}</li>
+                    <li>D:{{v['option_D']}}</li>
+                </ul>
+                答案:<el-tag>{{v['answer']}}</el-tag>
+            </el-collapse-item>
+        </el-collapse>
     </div>
 </template>
 
 <script setup>
-import Analysis from '../../components/Analysis.vue'
-
+import {ref} from 'vue'
+import {useRoute} from 'vue-router'
+import {getGradeAnalysis} from '../../api/teacher'
+const route=useRoute()
+const details=ref({})
+getGradeAnalysis(route.params.id).then((res)=>{
+    details.value=res.data
+    console.log(res.data);
+})
 </script>
 <script>
 export default {
-  components: { Analysis },
     name:"GradeAnalysis",
 }
 </script>
--- src/views/teacher/GradeOverview.vue
+++ src/views/teacher/GradeOverview.vue
@@ -3,6 +3,13 @@
         <el-card>
             参加人数 {{grade.length}} 最高分 {{maxScore}} 最低分 {{minScore}} 平均分 {{averageScore}}
         </el-card>
+
+        <div class="echarts">
+            <h1>成绩分布图</h1>
+            <div id="bar"></div>
+            <div id="pie"></div>
+        </div>
+
         <el-table :data="grade">
             <el-table-column prop="username" label="用户名" width="180" />
             <el-table-column prop="score" label="分数" width="180" />
@@ -11,12 +18,18 @@
 </template>
 
 <script setup >
-import {ref,computed} from 'vue'
+import {ref,computed,onMounted} from 'vue'
 import {useRoute} from 'vue-router'
+import * as echarts from 'echarts';
 import {getAllGrade} from '@/api/teacher'
+
 const route=useRoute()
 const grade=ref([])
 const score=ref([])
+const dataForCharts=ref({})
+const barX=ref([])
+const barY=ref([])
+const pie=ref([])
 
 let maxScore=computed(()=>{
     return Math.max.apply(null,score.value)
@@ -38,8 +51,72 @@ getAllGrade(route.params.id).then((res)=>{
         sc.push(parseInt(res.data[i]['score']))
     }
     score.value=sc
-    console.log(res.data);
+    for(let i in score.value){
+        let index=Math.floor(score.value[i]/20)
+        let name=`${20*index}-${20*(index+1)-1}`
+        if(dataForCharts.value[name]){
+            dataForCharts.value[name]+=1
+        }
+        else{
+            dataForCharts.value[name]=1
+        }
+    }
+    barX.value=Object.keys(dataForCharts.value)
+    barY.value=Object.values(dataForCharts.value)
+    for(let [k,v] of Object.entries(dataForCharts.value)){
+        pie.value.push({'name':k,'value':v})
+    }
+
+
+    load()
 })
+
+
+function load(){
+    var barChart=echarts.init(document.getElementById('bar'));
+    var pieChart=echarts.init(document.getElementById('pie'));
+    barChart.setOption({
+        xAxis: {
+            type: 'category',
+            data: barX.value
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+            data: barY.value,
+            type: 'bar'
+            }
+        ]
+    })
+    pieChart.setOption({
+        tooltip: {
+            trigger: 'item'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left'
+        },
+        series: [
+            {
+            name: 'Access From',
+            type: 'pie',
+            radius: '50%',
+            data: pie.value,
+            emphasis: {
+                itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+            }
+        ]
+    })
+}
+
+
 
 
 </script>
@@ -50,5 +127,10 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+#bar , #pie {
+    display: inline-block;
+    width: 350px;
+    height: 200px;
+    margin: 8px;
+}
 </style>
